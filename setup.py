ea52bec03b2ffea99db6f7bf108824ed2342312c
Erik Youngren <artanis.00@gmail.com>
2013-09-22 22:41:13 -0700

    Add setup.py and MANIFEST.in.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+__VERSION__ = "0.1.0-dev"
+
+setup(
+    # package information
+    name="icecrate",
+    version=__VERSION__,
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=[
+        # primary requires
+        "dobbin", "pyxdg", "bottle>=0.12-dev",
+        
+        # dobbin requires
+        "transaction", "zope.interface"],
+
+    dependency_links=[
+        # get bottle 0.12 from github
+        "https://github.com/defnull/bottle/tarball/master#egg=bottle-0.12-dev"],
+
+    # metadata
+    author="Erik Youngren",
+    author_email="artanis.00@gmail.com",
+    license="BSD 2-clause",
+    url="https://github.com/Artanis/icecrate"
+
+    )
