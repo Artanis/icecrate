54d1055754cc5b9774a2cc2639cecda19fb5f81f
Erik Youngren <artanis.00@gmail.com>
2013-10-11 17:44:52 -0700

    Add missing PyDispatcher dependency.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=[
-        "bottle>=0.12-dev", "redis"],
+        "bottle>=0.12-dev", "redis", "PyDispatcher"],
 
     dependency_links=[
         # get bottle 0.12 from github
